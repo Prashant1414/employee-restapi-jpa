6ef343c41b4c26ed04accbfae851a0db3cd45b4a
Prashant1414 <pshirale1@gmail.com>
2023-07-09 17:29:39 +0530

    initial commit

---
+++ employee-restapi-jpa/src/main/java/com/employee/api/EmployeeRestapiJpaApplication.java
@@ -0,0 +1,13 @@
+package com.employee.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRestapiJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRestapiJpaApplication.class, args);
+	}
+
+}
